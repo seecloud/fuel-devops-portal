f5c2e7e1fc6391540fe249f5783e605781cc2864
Vitaly Kramskikh <vkramskikh@mirantis.com>
2016-10-19 20:02:44 +0300

    Added webpack & babel

---
+++ webpack.config.babel.js
@@ -0,0 +1,35 @@
+import path from 'path';
+
+export default {
+  entry: [
+    'whatwg-fetch',
+    './src/app.js'
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    publicPath: '/',
+    filename: 'bundle.js',
+    chunkFilename: null,
+    sourceMapFilename: 'bundle.js.map'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {cacheDirectory: true}
+      },
+      {test: /\.css$/, loader: 'style!css'},
+      {test: /\.less$/, loader: 'style!css!less'},
+      {test: /\.(gif|png|jpg)$/, loader: 'file'},
+      {test: /\.(woff|woff2|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'}
+    ]
+  },
+  plugins: [],
+  devtool: 'cheap-module-source-map',
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000
+  }
+};
