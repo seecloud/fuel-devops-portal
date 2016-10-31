e1ab7091ba041de7385c1e1dd1d1581d7ac0ef48
Vitaly Kramskikh <vkramskikh@mirantis.com>
2016-11-01 00:46:44 +0300

    Use Firefox only for unit tests
    Tap reporter has issues with multiple browsers

--- karma.conf.babel.js
+++ karma.conf.babel.js
@@ -46,7 +46,7 @@ export default (config) => {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome'],
+    browsers: ['Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
