e3907b2b0d89e8d294231c7d8537e2f3404b92c5
Bogdan Dudko <bdudko@gmail.com>
2016-11-25 16:29:34 +0200

    New login screen

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -12,55 +12,60 @@ export default class LoginPage extends Component {
 
   onSubmit = (e) => {
     e.preventDefault();
-    this.actionInProgress = true;
     const {router, location, uiState} = this.props;
+    this.actionInProgress = true;
     setTimeout(() => {
       uiState.authenticated = true;
       router.replace(get(location, 'state.nextPathname', '/'));
-    }, 500);
+    }, 1000);
   }
 
   render() {
     return (
-      <div className='container-fluid'>
-        <h1>{'Login'}</h1>
-        <form className='form-horizontal' onSubmit={this.onSubmit}>
-          <div className='form-group'>
-            <div className='col-xs-6 col-xs-offset-3'>
-              <input
-                className='form-control input-sm'
-                type='text'
-                name='username'
-                ref='username'
-                placeholder={'Username'}
-                onChange={this.onChange}
-              />
-            </div>
+      <div className='container-fluid dark-bg'>
+        <div className='login-container'>
+          <div className='logo-container'>
+            <div className='logo' />
           </div>
-          <div className='form-group'>
-            <div className='col-xs-6 col-xs-offset-3'>
-              <input
-                className='form-control input-sm'
-                type='password'
-                name='password'
-                ref='password'
-                placeholder={'Password'}
-                onChange={this.onChange}
-              />
-            </div>
+          <div className='form-container'>
+            <h1>{'Log In'}</h1>
+            <form className='form-horizontal' onSubmit={this.onSubmit}>
+              <div className='form-group'>
+                <div className='col-xs-12'>
+                  <input
+                    className='form-control input-sm'
+                    type='text'
+                    name='username'
+                    ref='username'
+                    placeholder={'Username'}
+                  />
+                </div>
+              </div>
+              <div className='form-group'>
+                <div className='col-xs-12'>
+                  <input
+                    className='form-control input-sm'
+                    type='password'
+                    name='password'
+                    ref='password'
+                    placeholder={'Password'}
+                  />
+                </div>
+              </div>
+              <div className='form-group'>
+                <div className='submit-button'>
+                  <button
+                    type='submit'
+                    className='btn btn-primary btn-block'
+                    disabled={this.actionInProgress}
+                  >
+                    {'Log In'}
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-          <div className='form-group'>
-            <div className='col-xs-12 text-center'>
-              <button
-                type='submit'
-                className={'btn btn-success'}
-                disabled={this.actionInProgress}
-              >
-                {'Log In'}
-              </button>
-            </div>
-          </div>
-        </form>
+        </div>
       </div>
     );
   }
