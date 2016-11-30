ee7fd23e301f86d40cd331f1b46a29c69bd33505
Vitaly Kramskikh <vkramskikh@gmail.com>
2016-11-30 15:46:50 +0300

    Remove unused import

--- src/components/CloudStatusAvailabilityMultiRegionPage.js
+++ src/components/CloudStatusAvailabilityMultiRegionPage.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {observer} from 'mobx-react';
-import {observe} from 'mobx';
 
 import CloudStatusSidebar from './CloudStatusSidebar';
 import StatusDataPeriodPicker from './StatusDataPeriodPicker';
