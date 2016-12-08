8a742869da7f6d0a6a34e1de1b70dc1e9963c49a
Boris Pavlovic <boris@pavlovic.me>
2016-12-08 14:10:49 -0800

    Dashboard page

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -10,22 +10,29 @@ export default class DashboardPage extends Component {
           <DashboardPageLink
             to='/all-regions/status'
             title='Status'
+            text='API Availability, API Health, Key Operations Performance'
           />
           <DashboardPageLink
             to='/all-regions/intelligence'
             title='Intelligence'
+            text='Google like search engine accross all resources of all envs physical and
+            virtually'
           />
           <DashboardPageLink
             to='/all-regions/capacity'
             title='Capacity'
+            text='Capacity Managmenet, Planning and Utilization of resources per region and tenant'
           />
           <DashboardPageLink
             to='/all-regions/runbooks'
             title='Runbooks'
+            text='Keep all scripts related to enviorment here and run them on demand or regular
+            basis'
           />
           <DashboardPageLink
             to='/all-regions/security'
             title='Security'
+            text='Monitory Security violations in cloud resources, e.g. wide open security groups'
           />
         </div>
       </div>
@@ -33,14 +40,13 @@ export default class DashboardPage extends Component {
   }
 }
 
-const DashboardPageLink = ({to, title}) => {
+const DashboardPageLink = ({to, title, text}) => {
   return (
     <div className='dashboard-page-link-container'>
       <div className='dashboard-page-link'>
         <h3>{title}</h3>
         <div>
-          {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '}
-          {'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
+          {text}
         </div>
         <Link to={to} className='btn btn-primary'>{'Launch'}</Link>
       </div>
