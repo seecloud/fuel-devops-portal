858db6e6a4cdfaaa02f9e81c5b8b77cb312a6ff1
Julia Aranovich <jkirnosova@mirantis.com>
2016-12-16 14:20:09 +0300

    Display specified number of ticks on axis X
    Pass ticksNumber prop to LineChart component to display
    the specified number of ticks on axis X.
    Default ticks number is 5.

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {AutoScaleAxis, Line} from 'chartist';
+import {FixedScaleAxis, Line} from 'chartist';
 import {merge} from 'lodash';
 import cx from 'classnames';
 
@@ -7,20 +7,33 @@ export default class LineChart extends Component {
   static defaultProps = {
     defaultOptions: {
       showPoint: false,
-      axisX: {
-        type: AutoScaleAxis,
-        scaleMinSpace: 60,
-        onlyInteger: true
-      }
+      axisX: {type: FixedScaleAxis}
     },
-    defaultResponsiveOptions: {}
+    defaultResponsiveOptions: {},
+    ticksNumber: 5
+  }
+
+  getTicks = () => {
+    const ticks = this.props.data.series[0].reduce((result, {x: time}) => {
+      if (time.getMinutes() === 0) result.push(time);
+      return result;
+    }, []);
+    const step = Math.round(ticks.length / (this.props.ticksNumber - 1));
+    let result = [];
+    for (let i = 0; i < ticks.length; i = i + step) result.push(ticks[i]);
+    result.push(ticks[ticks.length - 1]);
+    return result;
+  }
+
+  componentWillMount() {
+    this.ticks = this.getTicks();
   }
 
   componentDidMount() {
     this.chartist = new Line(
       this.refs.chart,
       this.props.data,
-      merge(this.props.defaultOptions, this.props.options),
+      merge(this.props.defaultOptions, {axisX: {ticks: this.ticks}}, this.props.options),
       {...this.props.defaultResponsiveOptions, ...this.props.responsiveOptions}
     );
   }
@@ -28,7 +41,7 @@ export default class LineChart extends Component {
   componentWillReceiveProps() {
     this.chartist.update(
       this.props.data,
-      merge(this.props.defaultOptions, this.props.options),
+      merge(this.props.defaultOptions, {axisX: {ticks: this.ticks}}, this.props.options),
       {...this.props.defaultResponsiveOptions, ...this.props.responsiveOptions}
     );
   }
