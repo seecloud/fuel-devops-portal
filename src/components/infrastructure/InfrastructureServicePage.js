8d785bf003df5ab4758b652ba96209210d56a3c0
Vitaly Kramskikh <vkramskikh@mirantis.com>
2016-12-30 19:30:03 +0700

    Actually show infrastructure service frames

--- src/components/infrastructure/InfrastructureServicePage.js
+++ src/components/infrastructure/InfrastructureServicePage.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import {inject, observer} from 'mobx-react';
-import {toJS} from 'mobx';
-import {flattenDeep} from 'lodash';
 
 @inject('infrastructureServices')
 @observer
@@ -11,17 +9,46 @@ export default class InfrastructureServicePage extends Component {
     const infrastructureService = this.props.infrastructureServices.get(
       regionName, infrastructureServiceId
     );
+    const {urls} = infrastructureService;
 
     return (
       <div className='container-fluid'>
         <h1>{'Infrastructure: ' + regionName + ': ' + infrastructureService.title}</h1>
-        {flattenDeep(toJS(infrastructureService.urls)).map((url) => {
+        {urls.map((urlsRow, rowIndex) => {
           return (
-            <div key={url}>
-              {`<IFRAME src="${url}" />`}
+            <div key={infrastructureServiceId + rowIndex} className='row'>
+              {urlsRow.map((url, colIndex) => <InfrastructureServiceFrame
+                key={infrastructureServiceId + colIndex + url}
+                rowSize={urls[rowIndex].length}
+                url={url}
+              />)}
             </div>
           );
         })}
+      </div>
+    );
+  }
+}
+
+export class InfrastructureServiceFrame extends Component {
+  static defaultProps = {
+    classNamesByRowSize: {
+      1: 'col-xs-12',
+      2: 'col-xs-12 col-sm-6',
+      3: 'col-xs-12 col-sm-12 col-md-4',
+      default: 'col-xs-12 col-sm-6 col-md-3'
+    }
+  }
+
+  render() {
+    const {url, rowSize, classNamesByRowSize} = this.props;
+
+    return (
+      <div className={classNamesByRowSize[rowSize] || classNamesByRowSize.default}>
+        <iframe
+          width='100%'
+          src={url}
+        />
       </div>
     );
   }
