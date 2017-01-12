7e47cf1fb16f61c02574967559712d84b38789e9
Vitaly Kramskikh <vkramskikh@mirantis.com>
2017-01-12 17:41:16 +0700

    Stay on the previous page in case fetchData failed for the next route

--- src/stores/UIState.js
+++ src/stores/UIState.js
@@ -2,6 +2,7 @@ import {observable} from 'mobx';
 import {PERIODS} from '../consts';
 
 export default class UIState {
+  @observable previousPathname = null;
   @observable authenticated = true;
   @observable pendingRequestsCount = 0;
   @observable activeStatusDataPeriod = PERIODS[0];
